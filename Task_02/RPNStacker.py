dd9e81531cad25bc73718bef3d1d8413d43c0e6b
Lucas <lucasvanlume@hotmail.com>
2023-04-20 22:09:59 -0300

    pequeno ajuste na lista de tokens

--- Task_02/RPNStacker.py
+++ Task_02/RPNStacker.py
@@ -6,7 +6,7 @@ def scan(operation):
         if token.isdigit():
             tokens.append({'type': 'NUM', 'lexame': int(token)})
         elif token in OPERATORS:
-            tokens.append({'type': 'OP', 'lexame': OPERATORS[token]})
+            tokens.append({'type': OPERATORS[token], 'lexame': token})
         else:
             raise ValueError('Unexpected character: {}'.format(token))
     return tokens
@@ -22,19 +22,18 @@ def rpn(tokens):
     for token in tokens:
         if token['type'] == 'NUM':
             push(stack, token['lexame'])
-        elif token['type'] == 'OP':
-            if token['lexame'] == 'PLUS':
-                push(stack, pop(stack) + pop(stack))
-            elif token['lexame'] == 'MINUS':
-                dig2 = pop(stack)
-                dig1 = pop(stack)
-                push(stack, dig1 - dig2)
-            elif token['lexame'] == 'STAR':
-                push(stack, pop(stack) * pop(stack))
-            elif token['lexame'] == 'SLASH':
-                dig2 = pop(stack)
-                dig1 = pop(stack)
-                push(stack, dig1 / dig2)
+        elif token['type'] == 'PLUS':
+            push(stack, pop(stack) + pop(stack))
+        elif token['type'] == 'MINUS':
+            dig2 = pop(stack)
+            dig1 = pop(stack)
+            push(stack, dig1 - dig2)
+        elif token['type'] == 'STAR':
+            push(stack, pop(stack) * pop(stack))
+        elif token['type'] == 'SLASH':
+            dig2 = pop(stack)
+            dig1 = pop(stack)
+            push(stack, dig1 / dig2)
     return pop(stack)
 
 with open('Calc2.stk', 'r') as f:
